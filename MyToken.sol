728d9404b86b5fcde018aa50dc5db525153d65cc
Dang Quoc Huy <huysamdua@yahoo.com>
2017-09-29 10:07:15 +0700

    internal method transfer

--- MyToken.sol
+++ MyToken.sol
@@ -17,15 +17,26 @@ contract MyToken {
     decimals = decimalUnits;                  // Amount of decimals for display purposes
 	}
 
-	function transfer(address _to, uint256 _value) {
-		// Check if sender has balance and for overflows
-		require(balanceOf[msg.sender] >= _value && balanceOf[_to] + _value >= balanceOf[_to]);
+	// function transfer(address _to, uint256 _value) {
+	// 	// Check if sender has balance and for overflows
+	// 	require(balanceOf[msg.sender] >= _value && balanceOf[_to] + _value >= balanceOf[_to]);
 
-		// Add and subtract new balances
-		balanceOf[msg.sender] -= _value;
-		balanceOf[_to] += _value;
+	// 	// Add and subtract new balances
+	// 	balanceOf[msg.sender] -= _value;
+	// 	balanceOf[_to] += _value;
 
-		// Notify anyone listening that this transfer took place
-		Transfer(msg.sender, _to, _value);
+	// 	// Notify anyone listening that this transfer took place
+	// 	Transfer(msg.sender, _to, _value);
+	// }
+
+	function _transfer(address _from, address _to, uint _value) internal {
+    require(_to != 0x0);                                // Prevent transfer to 0x0 address. Use burn() instead
+    require(balanceOf[_from] >= _value);                 // Check if the sender has enough
+    require(balanceOf[_to] + _value >= balanceOf[_to]);  // Check for overflows
+    require(!frozenAccount[_from]);                     // Check if sender is frozen
+    require(!frozenAccount[_to]);                       // Check if recipient is frozen
+    balanceOf[_from] -= _value;                         // Subtract from the sender
+    balanceOf[_to] += _value;                           // Add the same to the recipient
+    Transfer(_from, _to, _value);
 	}
 }
