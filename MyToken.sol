036eb35f26ff7e751e3375d0852b6530888589ea
Dang Quoc Huy <huysamdua@yahoo.com>
2017-09-29 10:32:44 +0700

    AUTOMATIC SELLING AND BUYING

--- MyToken.sol
+++ MyToken.sol
@@ -26,6 +26,8 @@ contract MyToken is owned {
 	string public symbol;
 	uint8 public decimals;
 	uint256 public totalSupply;
+	uint256 public sellPrice;
+	uint256 public buyPrice;
 
 	event Transfer(address indexed from, address indexed to, uint256 value);
 	event FrozenFunds(address target, bool frozen);
@@ -55,6 +57,30 @@ contract MyToken is owned {
 	function freezeAccount(address target, bool freeze) onlyOwner {
 		frozenAccount[target] = freeze;
 		FrozenFunds(target, freeze);
+	}
+
+	function setPrices(uint256 newSellPrice, uint256 newBuyPrice) onlyOwner {
+		sellPrice = newSellPrice;
+		buyPrice = newBuyPrice;
+	}
+
+	function buy() payable returns (uint amount){
+		amount = msg.value / buyPrice;                    // calculates the amount
+		require(balanceOf[this] >= amount);               // checks if it has enough to sell
+		balanceOf[msg.sender] += amount;                  // adds the amount to buyer's balance
+		balanceOf[this] -= amount;                        // subtracts amount from seller's balance
+		Transfer(this, msg.sender, amount);               // execute an event reflecting the change
+		return amount;                                    // ends function and returns
+	}
+
+	function sell(uint amount) returns (uint revenue){
+		require(balanceOf[msg.sender] >= amount);         // checks if the sender has enough to sell
+		balanceOf[this] += amount;                        // adds the amount to owner's balance
+		balanceOf[msg.sender] -= amount;                  // subtracts the amount from seller's balance
+		revenue = amount * sellPrice;
+		require(msg.sender.send(revenue));                // sends ether to the seller: it's important to do this last to prevent recursion attacks
+		Transfer(msg.sender, this, amount);               // executes an event reflecting on the change
+		return revenue;                                   // ends function and returns
 	}
 
 	// function transfer(address _to, uint256 _value) {
